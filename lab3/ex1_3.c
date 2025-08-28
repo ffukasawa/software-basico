e422c67b92d938f96fc591abfd84cb2ae8adf4a7
ffukasawa <fefs.fuka@gmail.com>
2025-08-28 13:42:42 +0000

    LAB3 P1

---
+++ lab3/ex1_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+
+  unsigned int x = 0x87654321;
+  //1000 0111 0110 0101 0100 0011 0010 0001(x)
+  unsigned int y, z;
+
+  /* o byte menos significativo de x e os outros bits em 0 */
+  //0000 0000 0000 0000 0000 0000 1111 1111(y)
+  y = x & 0xFF; //y = 0x000000FF;
+
+  /* o byte mais significativo com todos os bits em '1' 
+     e os outros bytes com o mesmo valor dos bytes de x */
+  z = x | 0xFF000000;
+
+  printf("%08x %08x\n", y, z);
+}
+
+
+
+
+
+
