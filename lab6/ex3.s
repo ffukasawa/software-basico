55e8162af4387b5357cd93c67d6d3489ede48cbf
ffukasawa <fefs.fuka@gmail.com>
2025-10-13 01:41:36 +0000

    lab6 finalizado

--- lab6/ex3.s
+++ lab6/ex3.s
@@ -2,10 +2,9 @@
 int nums[] = {10, -21, -30, 45};
 int main() {
   int i, *p;
-  int sum = 0;
   for (i = 0, p = nums; i != 4; i++, p++)
-    sum += *p;
-  printf("%d\n", sum);
+    if ((*p % 2) == 0)
+      printf("%d\n", *p);
   return 0;
 }
 */
@@ -13,23 +12,17 @@ int main() {
 /*
 Dicionário
 Registrador Variável
-ebx         i
-r12         *p
-ecx         sum
-         
+%ebx        i
+%r12        *p
 */
 
-#seguimento de dados(inicializados)
 .data
-nums:  .int  10, -21, -30, 45
-Sf:  .string "%d\n"    # string de formato para printf
+nums: .int 10, -21, -30, 45
+Sf: .string "%d\n"
 
-#seguimento de texto(instruções)
 .text
-#definição de símbolo global
-.globl  main
+.globl main
 main:
-
 /********************************************************/
 /* mantenha este trecho aqui e nao mexa - prologo !!!   */
   pushq   %rbp
@@ -39,29 +32,36 @@ main:
   movq    %r12, -16(%rbp)
 /********************************************************/
 
-  movl  $0, %ebx  /* ebx = 0;  ebx como variavel i*/
-  movq  $nums, %r12  /* r12 = &nums; r12 é a variavel p*/
-  movl  $0, %ecx /* int sum = 0;*/
+movl $0, %ebx
+movq $nums, %r12
 
 L1:
-  cmpl  $4, %ebx  /* if (ebx == 4) ? */
-  je  L2          /* goto L2 */
+cmpl $4, %ebx
+je L3
 
-  addl  (%r12), %ecx /* sum += *p; */
-  addl  $1, %ebx  /* ebx += 1; i++ */
-  addq  $4, %r12  /* r12 += 4; p++ (somar 4 quando é ponteiro) */
-  jmp  L1         /* goto L1; */
+movl (%r12), %eax       # lê o número *p
+movl %eax, %edx         # copia para edx (teste)
+andl $1, %edx           # testa o bit menos significativo
+cmpl $0, %edx           # se resultado == 0 -> par
+je   L2                 # se == 0 -> par (jump if equal to L2)
 
-L2: 
- movl  %ecx, %eax  /* eax = ecx */
+addl $1, %ebx
+addq $4, %r12
+jmp L1
 
+
+L2:
 /*************************************************************/
 /* este trecho imprime o valor de %eax (estraga %eax)  */
   movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
   movl    %eax, %esi   /* segundo parametro  (inteiro) */
   call  printf       /* chama a funcao da biblioteca */
 /*************************************************************/
+addl $1, %ebx              # i++
+addq $4, %r12              # p++
+jmp L1
 
+L3:
 /***************************************************************/
 /* mantenha este trecho aqui e nao mexa - finalizacao!!!!      */
   movq  $0, %rax  /* rax = 0  (valor de retorno) */
