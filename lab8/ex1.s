27d026ec03bb583312798339dd9242dbf8a77d0e
ffukasawa <fefs.fuka@gmail.com>
2025-10-13 05:36:50 +0000

    iniciando o lab8

---
+++ lab8/ex1.s
@@ -0,0 +1,23 @@
+/*
+ #include <stdlib.h>
+  int main() {
+    system("ls -ls");
+    return 0;
+  }
+*/
+
+.data
+  s1: .string "ls -ls"
+
+  .text
+  .globl main
+  main:
+  /* prologo */
+    pushq %rbp
+    movq  %rsp, %rbp
+
+  /* coloque seu codigo aqui */
+
+  /* finalizacao */
+    leave
+    ret
